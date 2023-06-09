a52ff6b5a3876e315d04fbb096059919a1b12d86
Keit Oliveira <keit.oliveira@icloud.com>
2023-06-08 22:01:28 -0300

    chore: live 2

--- devnaestrada/src/app/layout.tsx
+++ devnaestrada/src/app/layout.tsx
@@ -1,5 +1,6 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
+import clsx from 'clsx';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={clsx(
+          inter.className,
+          `
+            bg-white
+          `
+        )}
+      >
+        {children}
+      </body>
     </html>
   )
 }
--- devnaestrada/src/app/page.tsx
+++ devnaestrada/src/app/page.tsx
@@ -1,19 +1,27 @@
 import Image from 'next/image'
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
-      <header>
+      <header
+        className={`
+          h-[80px]
+        `}
+      >
         <div
           className={`
             container
             mx-auto
+            h-full
           `}
         >
           <div
             className={`
               grid
               grid-cols-1
+              h-full
+              items-center
               lg:grid-cols-2
             `}
           >
@@ -25,33 +33,88 @@ export default function Home() {
               className={`
                 grid
                 grid-cols-1
-                lg:grid-cols-2
+                lg:grid-flow-col
+                lg:auto-cols-max
               `}
             >
-              <nav>
-                <ul>
-                  <li>
-                    <a href="#">Home</a>
-                  </li>
-                  <li>
-                    <a href="#">Sobre</a>
-                  </li>
-                  <li>
-                    <a href="#">Episódios</a>
-                  </li>
-                  <li>
-                    <a href="#">Contato</a>
-                  </li>
+              <nav
+                className={`
+                  mr-8
+                `}
+              >
+                <ul
+                  className={`
+                    grid
+                    grid-flow-col
+                    auto-cols-max
+                    gap-x-3
+                    h-full
+                    items-center
+                    justify-end
+                  `}
+                >
+                  {[
+                    {
+                      id: 1,
+                      label: 'Home',
+                      href: '#',
+                      target: '_self',
+                      a11y: 'Ir para a página inicial',
+                    },
+                    {
+                      id: 2,
+                      label: 'Sobre',
+                      href: '#',
+                      target: '_self',
+                      a11y: 'Ir para a página sobre nós',
+                    },
+                    {
+                      id: 3,
+                      label: 'Episódios',
+                      href: '#',
+                      target: '_self',
+                      a11y: 'Ir para a página Episódios',
+                    },
+                    {
+                      id: 4,
+                      label: 'Contato',
+                      href: '#',
+                      target: '_self',
+                      a11y: 'Ir para a página Contato',
+                    },
+                  ].map(({ id, ...link }) => (
+                    <li key={id}>
+                      <Link
+                        href={link.href}
+                        aria-label={link.a11y}
+                        target={link.target}
+                        className={`
+                          focus:font-bold
+                          outline-none
+                        `}
+                      >
+                        {link.label}
+                      </Link>
+                    </li>
+                  ))}
                 </ul>
               </nav>
 
               <div>
-                <a
-                  href="#"
+                <Link
+                  href="https://apoia.se/frontin"
                   target="_blank"
+                  className={`
+                    py-2
+                    px-4
+                    inline-block
+                    bg-[green]
+                    text-black
+                    rounded-sm
+                  `}
                 >
                   Apoia-se
-                </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -59,14 +122,25 @@ export default function Home() {
       </header>
 
       <main>
-        <div>
+        {/* BANNER */}
+        <div
+          className={`
+            bg-black
+            text-white
+          `}
+        >
           <div
             className={`
               container
               mx-auto
             `}
           >
-            <div>
+            <div
+              className={`
+                py-8
+                text-center
+              `}
+            >
               <h2
                 className={`
                   text-6xl
@@ -89,26 +163,102 @@ export default function Home() {
           </div>
         </div>
 
-        <div>
+        {/* MAIN */}
+        <div
+          className={`
+            py-10
+          `}
+        >
           <div
             className={`
               container
               mx-auto
             `}
           >
-            MAIN
+            {/* SEARCH */}
+            <div
+              className={`
+                grid
+                justify-end
+                lg:grid-flow-col
+                lg:auto-cols-max
+              `}
+            >
+              <form
+                action="#"
+                role="search"
+                className={`
+                  w-[50vw]
+                `}
+              >
+                <input
+                  type="text"
+                  aria-label="Buscar episódios"
+                  placeholder="Buscar episódios"
+                  className={`
+                    border-2
+                    border-black
+                  `}
+                />
+                <button
+                  type="submit"
+                >
+                  Buscar
+                </button>
+              </form>
+            </div>
+
+
+
           </div>
         </div>
       </main>
 
-      <footer>
+      <footer
+        className={`
+          bg-black
+          text-white
+        `}
+      >
         <div
           className={`
             container
             mx-auto
           `}
         >
-          wrapper
+          <div
+            className={`
+              grid
+              grid-cols-1
+              py-10
+              lg:grid-cols-3
+            `}
+          >
+            <div
+              className={`
+
+              `}
+            >
+              ITEM 1
+
+            </div>
+            <div
+              className={`
+
+              `}
+            >
+              ITEM 2
+
+            </div>
+            <div
+              className={`
+
+              `}
+            >
+              ITEM 3
+
+            </div>
+          </div>
         </div>
       </footer>
     </>
