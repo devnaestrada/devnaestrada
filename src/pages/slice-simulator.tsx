3e5eafa11ef3f8d16786354977eb78016775d889
Keit Oliveira <keit.oliveira@Keits-MacBook-Pro.local>
2023-07-06 23:31:07 -0300

    build: remove unused packages

--- src/pages/slice-simulator.tsx
+++ src/pages/slice-simulator.tsx
@@ -1,7 +1,7 @@
 import { SliceSimulator } from "@slicemachine/adapter-next/simulator";
 import { SliceZone } from "@prismicio/react";
 
-import { components } from "../slices";
+import { components } from '../../slices';
 
 export default function SliceSimulatorPage() {
   return (
